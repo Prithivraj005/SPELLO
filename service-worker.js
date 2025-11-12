713af5196fcd0db14280a29077b778bc245d9178
Prithivraj005 <005prithivraj@gmail.com>
2025-11-13 01:49:11 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("spello-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/styles.css",
+        "/script.js",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
